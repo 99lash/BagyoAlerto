b2326527636e288842d37db8dd6c8de055a1452b
GABlane <ofiangga.bscs@gmail.com>
2025-08-05 23:12:49 +0800

    initial function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
+alert = document.querySelector("alert warning").innerHTML = `<label for="alert-close" class="alert-close-btn" aria-label="Dismiss alert">&times;</label>
+
+          <div class="alert-icon">
+            <i class="ph ph-warning"></i>
+          </div>
+
+          <div class="alert-content">
+            <div class="alert-header">
+              Typhoon Warning
+            </div>
+            <p>
+              Typhoon "Bagyo" is approaching Metro Manila. Expected landfall in 18 hours. Prepare your emergency kit now.
+            </p>
+            <span class="alert-timestamp">Jul 28, 2025, 09:22 PM</span>
+          </div>`
+
 function SwitchMode(){
       document.documentElement.classList.toggle('dark');
+}
+
+
+function addAlert(){
+
 }
