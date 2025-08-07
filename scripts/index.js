8d3f0d0e3c8644995b42ff57041d064c8a31409a
GABlane <ofiangga.bscs@gmail.com>
2025-08-08 02:03:59 +0800

    deleled the comments because I know the problem

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,6 @@ import { SwitchThemeMode } from "./features/theme.js";
 import { renderAlertReminder } from "./features/alert.js";
 
 
-window.showToast = showToast; // sher di ko alam if bakit di pwede yung normal na showToast(msg), if may possible solution na iba gawa nalang ng issue
+window.showToast = showToast;
 SwitchThemeMode();
 renderAlertReminder();
