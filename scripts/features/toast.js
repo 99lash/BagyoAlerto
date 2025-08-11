93df8c863daa96f41a48ae54a2ffdd4868b39c96
GABlane <ofiangga.bscs@gmail.com>
2025-08-11 18:53:13 +0800

    added icons

--- scripts/features/toast.js
+++ scripts/features/toast.js
@@ -1,11 +1,17 @@
-export function showToast(){
-    document.querySelectorAll(".copy").forEach((btn, index) => { 
+export function showToast() {
+    document.querySelectorAll(".copy").forEach((btn, index) => {
         btn.addEventListener("click", () => {
             const text = document.querySelectorAll(".number")[index].textContent.trim();
+            const msg = " Copied " + text;
+
+
 
             const toast = document.createElement('div');
             toast.classList.add('toast');
-            toast.innerText = "SUCCESSFULLY COPIED " + text;
+            const icon = document.createElement('i');
+            icon.className = 'ph ph-copy';
+            toast.appendChild(icon);
+            toast.appendChild(document.createTextNode(msg));
             toastBox.appendChild(toast);
 
             setTimeout(() => {
@@ -14,18 +20,22 @@ export function showToast(){
         })
     });
 
-    document.querySelectorAll(".call").forEach((btn, index) => { 
+    document.querySelectorAll(".call").forEach((btn, index) => {
         btn.addEventListener("click", () => {
             const text = document.querySelectorAll(".number")[index].textContent.trim();
+            const msg = " Calling " + text;
 
             const toast = document.createElement('div');
             toast.classList.add('toast');
-            toast.innerText = "CALLING " + text;
+            const icon = document.createElement('i');
+            icon.className = 'ph ph-phone';
+            toast.appendChild(icon);
+            toast.appendChild(document.createTextNode(msg));
             toastBox.appendChild(toast);
 
             setTimeout(() => {
                 toast.remove();
             }, 3000);
-        })
+        });
     });
 }
