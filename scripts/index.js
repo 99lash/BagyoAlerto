6c31349229c445f5911620424b21bfe00e66be4f
GABlane <ofiangga.bscs@gmail.com>
2025-08-12 13:22:55 +0800

    resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,7 @@
 import { showToast } from "./features/toast.js";
 import { SwitchThemeMode } from "./features/theme.js";
 import { renderAlertReminder } from "./features/alert.js";
-import { copyButton } from "./features/emergency-contacts.js";
-import { callButton } from "./features/emergency-contacts.js";
+import { copyButton ,callButton} from "./features/emergency-contacts.js";
 
 showToast();
 SwitchThemeMode();
